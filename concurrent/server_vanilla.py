0d30a299aac1c9c9b52f165cccdfd8d34e5d04fb
xiangqi <xiangqi@kuaishou.com>
2026-07-02 20:22:07 +0800

    第一版

--- concurrent/server_vanilla.py
+++ concurrent/server_vanilla.py
@@ -3,9 +3,30 @@ from socket import *
 
 def server(address):
     sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
     sock.bind(address)
     sock.listen(5)
 
     while True:
         client, addr = sock.accept() # block
+        print('Client connected from', addr)
+        fib_handler(client)
 
+def fib_handler(client):
+    while True:
+        req = client.recv(1024)
+        if not req:
+            break
+        n = int(req)
+        result = fib(n)
+        resp = str(result).encode("ascii") + b'\n'
+        client.send(resp)
+    print('Client disconnected')
+
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+server(("", 25565))
